3a8382caeabf91d083624a5ea14e214387759c97
Mike Johansson <mikejoh12@gmail.com>
2022-06-02 22:02:44 -0500

    fix TS related capitalization issue with import

--- src/helpers/get3dBoard.test.ts
+++ src/helpers/get3dBoard.test.ts
@@ -1,5 +1,5 @@
 import { get3dBoard } from "./get3dBoard";
-import { threeDBoard } from "./get3dboard.testdata";
+import { threeDBoard } from "./get3dBoard.testdata";
 
 test(`get3DdBoard should return array of possible board [x,y,z] positions`, () => {
     expect(get3dBoard()).toStrictEqual(threeDBoard);
